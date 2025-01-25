465853170e6c777e88cebc2ba7ab69f2e91a8956
Elias <eliaswhe@stud.ntnu.no>
2025-01-25 18:07:31 +0100

    Course page init

--- pages/courses/index.tsx
+++ pages/courses/index.tsx
@@ -1,5 +1,6 @@
 import { title } from "@/components/primitives";
 import DefaultLayout from "@/layouts/default";
+import ProgressCard from "@/components/progressCard";
 
 export default function DocsPage() {
   return (
@@ -7,8 +8,12 @@ export default function DocsPage() {
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
         <div className="inline-block max-w-lg text-center justify-center">
           <h1 className={title()}>Docs</h1>
+          
         </div>
       </section>
+      <div className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <ProgressCard />
+      </div>
     </DefaultLayout>
   );
 }
